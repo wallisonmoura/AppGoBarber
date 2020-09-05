1cfffc8e310f18cc4b9710943dd9b170c7ebe57a
wallisonmoura <wallison_cedaspy@hotmail.com>
2020-09-05 17:54:17 -0300

    Context API

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -5,10 +5,14 @@ import SignIn from './pages/SignIn';
 
 import GlobalStyle from './styles/global';
 
+import AuthContext from './context/AuthContext';
+
 const App: React.FC = () => {
   return (
     <>
-      <SignIn />
+      <AuthContext.Provider value={{ name: 'Wallison' }}>
+        <SignIn />
+      </AuthContext.Provider>
       <GlobalStyle />
     </>
   );
